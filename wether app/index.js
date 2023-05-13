21923d546667b6642ddab3e0082bde075adeff3c
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-05-13 23:35:43 +0530

    Add files via upload

---
+++ wether app/index.js
@@ -0,0 +1,38 @@
+const apikey = "a411361d72fbec7fc9fd3ea3fbff51d9";
+const apiurl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+async function cheackWeather(city) {
+  const response = await fetch(apiurl + city + `&appid=${apikey}`);
+  var data = await response.json();
+
+ 
+
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "img/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "img/clear.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "img/rain.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "img/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "img/miist.png";
+  }
+
+  document.querySelector(".weather").style.display="block";
+}
+
+searchBtn.addEventListener("click", () => {
+  cheackWeather(searchBox.value);
+});
